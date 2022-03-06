1be4f90108caad844c6f0b953ebc85c60b5e61d1
Levi Freire <levifreire7@gmail.com>
2022-03-06 16:40:45 -0300

    criacao de objetos na classe principal

--- src/br/com/levifreire/desafio/Main.java
+++ src/br/com/levifreire/desafio/Main.java
@@ -8,21 +8,41 @@ public class Main {
 		Curso curso1 = new Curso();
 		curso1.setTitulo("Curso Java");
 		curso1.setDescricao("Descrição curso Java");
-		curso1.setCargaHoraria(8);		
-		System.out.println(curso1);
-		
+		curso1.setCargaHoraria(8);
+
 		Curso curso2 = new Curso();
 		curso2.setTitulo("Curso Python");
 		curso2.setDescricao("Descrição curso Python");
 		curso2.setCargaHoraria(8);
-		System.out.println(curso2);
-		
+
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("Mentoria Java");
 		mentoria.setDescricao("Descrição mentoria Java");
 		mentoria.setData(LocalDate.now());
-		System.out.println(mentoria);
 
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+
+		Dev devJoao = new Dev();
+		devJoao.setNome("João");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Joao: " + devJoao.getConteudosInscritos());
+		devJoao.progredir();
+		System.out.println("Conteúdos Concluidos Joao: " + devJoao.getConteudosConcluidos());
+		System.out.println("XP: " + devJoao.calcularTotalXp());
+
+		Dev devMaria = new Dev();
+		devMaria.setNome("Maria");
+		devMaria.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Maria: " + devMaria.getConteudosInscritos());
+		devMaria.progredir();
+		devMaria.progredir();
+		System.out.println("Conteúdos Concluidos Maria: " + devMaria.getConteudosConcluidos());
+		System.out.println("XP: " + devMaria.calcularTotalXp());
 	}
 
 }
